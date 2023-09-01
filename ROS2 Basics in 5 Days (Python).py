9664e26bcff1e263f75f1c5a1c50b25b73e723b5
DahyunLee1023 <143322688+DahyunLee1023@users.noreply.github.com>
2023-09-01 22:30:52 +0900

    Create ROS2 Basics in 5 Days (Python).py

---
+++ ROS2 Basics in 5 Days (Python).py
@@ -0,0 +1,5 @@
+#This is the knowledge that I learnt through the course "ROS2 Basics in 5 Days (Python)"
+
+#Sept 1st
+#ROS2vs ROS -> ROS2 features: DDS communication, real-time features, embedded security, and it's better prepared for the industrial world.
+#To get a certificate, I need to complete a live presentation on my project!!
